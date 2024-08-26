a3679df0890c754664928022f24f34438eda0137
vikash kumar <9899626716a@gmail.com>
2024-08-26 15:33:00 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
 
-import Home from './page/home.jsx'
+
 
 import Headers from './components/Headers.jsx'
 import Register from './page/Register.jsx'
 import Login from './page/Login.jsx'
 import Profile from './page/Profile.jsx'
 import { Toaster } from 'react-hot-toast'
+import Hno from './page/Hno.jsx'
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
      <Headers/>
     <Routes>
      
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Hno/>}/>
       <Route path='profile' element={<Profile/>}/>
       <Route path='login' element={<Login/>}/>
       <Route path='register' element={<Register/>}/>
---
+++ src/page/Hno.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hno() {
+  return (
+    <div>Hno</div>
+  )
+}
+
+export default Hno
